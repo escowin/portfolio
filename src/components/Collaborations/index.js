c8574dc9bac53e693f83b5202a2a8dcf7e70f09d
escowin <edwin@escowinart.com>
2022-04-25 01:52:25 -0500

    <SoloWork> reworked for {display: grid}, <Collaborations> reworked for full width. temp hardcode in place as a visual guide

--- src/components/Collaborations/index.js
+++ src/components/Collaborations/index.js
@@ -1,26 +1,53 @@
 import React from "react";
-import Placeholder from '../../assets/images/placeholder.jpg';
+import Placeholder from '../../assets/images/placeholder-3.jpg';
 
 // IN FINAL VERSION, CARDS WILL NOT BE HARDCODED. WILL BE PULLED AND GENERATED DYNAMICALLY.
 function Collaborations() {
     return (
-      <article className='sections articles' id='collaborations'>
+      <section className='sections articles' id='collaborations'>
         <h3>Collaborations</h3>
         {/* GROUP PROJECTS | SCREENSHOT & TEXT */}
-        <div className="card">
-          <div className="card-img">
-            <img src={Placeholder} alt="placeholder" />
-          </div>
-          <div className="card-body">
-            <h4>Project Title</h4>
-            <p><a href="/">Github Repo</a> | <a href="/">Live URL</a></p>
-            <p>html / css / etc</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, provident? Velit reprehenderit natus accusantium delectus sed libero soluta voluptate nam architecto, assumenda eveniet harum consequuntur totam unde enim odio deleniti.
-          </p>
-          </div>
-        </div>
+        <div className="collaborations">
+          <article className="card group-projects">
+            <div className="card-img">
+              <img src={Placeholder} alt="placeholder" />
+            </div>
+            <div className="card-body">
+              <h4>Project Title</h4>
+              <p><a href="/">Github Repo</a> | <a href="/">Live URL</a></p>
+              <p>html / css / etc</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, provident? Velit reprehenderit natus accusantium delectus sed libero soluta voluptate nam architecto, assumenda eveniet harum consequuntur totam unde enim odio deleniti.
+            </p>
+            </div>
+          </article>
 
-      </article>
+          <article className="card group-projects">
+            <div className="card-img">
+              <img src={Placeholder} alt="placeholder" />
+            </div>
+            <div className="card-body">
+              <h4>Project Title</h4>
+              <p><a href="/">Github Repo</a> | <a href="/">Live URL</a></p>
+              <p>html / css / etc</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, provident? Velit reprehenderit natus accusantium delectus sed libero soluta voluptate nam architecto, assumenda eveniet harum consequuntur totam unde enim odio deleniti.
+            </p>
+            </div>
+          </article>
+
+          <article className="card group-projects">
+            <div className="card-img">
+              <img src={Placeholder} alt="placeholder" />
+            </div>
+            <div className="card-body">
+              <h4>Project Title</h4>
+              <p><a href="/">Github Repo</a> | <a href="/">Live URL</a></p>
+              <p>html / css / etc</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, provident? Velit reprehenderit natus accusantium delectus sed libero soluta voluptate nam architecto, assumenda eveniet harum consequuntur totam unde enim odio deleniti.
+            </p>
+            </div>
+          </article>
+        </div>
+      </section>
     );
   }
   export default Collaborations;
--- src/components/SoloWork/index.js
+++ src/components/SoloWork/index.js
@@ -1,24 +1,77 @@
 import React from "react";
-import Placeholder from "../../assets/images/placeholder.jpg";
+import Placeholder1 from "../../assets/images/placeholder-1.jpg";
+import Placeholder2 from "../../assets/images/placeholder-2.jpg";
 
 function SoloWork() {
     return (
-      <article className='sections articles' id='solo-work'>
+      <section className='sections articles' id='solo-work'>
         <h3>Solo Work</h3>
-        {/* Solo Work will have a featured project that takes up the width of this article. underneath the featured project will be <div className="cards-container"> which will contain all non-featured projects. <.cards-container> will be in grid layout. desktop view fits 4 columns, tablet & mobile 2. non-featured cards will have {display: inline-block}. Non featured cards wont have a description <p>text  */}
-        <div className="card">
-          <div className="card-img">
-            <img src={Placeholder} alt="placeholder" />
-          </div>
-          <div className="card-body">
-            <h4>Solo Project Title</h4>
-            <p><a href="/">Github Repo</a> | <a href="/">Live URL</a></p>
-            <p>html / css / etc</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, provident? Velit reprehenderit natus accusantium delectus sed libero soluta voluptate nam architecto, assumenda eveniet harum consequuntur totam unde enim odio deleniti.
-          </p>
-          </div>
+        {/* SOLO WORK | FEATURED */}
+        {/* FEATURED & NON-FEATURED PROJECT CONTENTS TO BE DYNAMICALLY GENERATED. HARDCODED IMG & TEXT FOR VISUAL REF. */}
+        <div className="grid-container">
+          <article className="card featured-project">
+            <div className="card-img">
+              <img src={Placeholder1} alt="placeholder" />
+            </div>
+            <div className="card-body">
+              <h4>Featured Solo Project Title</h4>
+              <p><a href="/">Github Repo</a> | <a href="/">Live URL</a></p>
+              <p>html / css / etc</p>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, provident? Velit reprehenderit natus accusantium delectus sed libero soluta voluptate nam architecto, assumenda eveniet harum consequuntur totam unde enim odio deleniti.
+            </p>
+            </div>
+          </article> 
+
+          {/* SOLO WORK | NON-FEATURED */}
+          <article className="card non-featured">
+            <div className="card-img">
+              <img src={Placeholder2} alt="placeholder" />
+            </div>
+            <div className="card-body">
+              <h4>Solo Project Title</h4>
+              <p><a href="/">Github Repo</a> | <a href="/">Live URL</a></p>
+              <p>html / css / etc</p>
+              <p><span className="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, provident? Velit reprehenderit natus accusantium delectus sed libero soluta voluptate nam architecto, assumenda eveniet harum consequuntur totam unde enim odio deleniti.</span></p>
+            </div>
+          </article> 
+
+          <article className="card non-featured">
+            <div className="card-img">
+              <img src={Placeholder2} alt="placeholder" />
+            </div>
+            <div className="card-body">
+              <h4>Solo Project Title</h4>
+              <p><a href="/">Github Repo</a> | <a href="/">Live URL</a></p>
+              <p>html / css / etc</p>
+              <p><span className="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, provident? Velit reprehenderit natus accusantium delectus sed libero soluta voluptate nam architecto, assumenda eveniet harum consequuntur totam unde enim odio deleniti.</span></p>
+            </div>
+          </article> 
+
+          <article className="card non-featured">
+            <div className="card-img">
+              <img src={Placeholder2} alt="placeholder" />
+            </div>
+            <div className="card-body">
+              <h4>Solo Project Title</h4>
+              <p><a href="/">Github Repo</a> | <a href="/">Live URL</a></p>
+              <p>html / css / etc</p>
+              <p><span className="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, provident? Velit reprehenderit natus accusantium delectus sed libero soluta voluptate nam architecto, assumenda eveniet harum consequuntur totam unde enim odio deleniti.</span></p>
+            </div>
+          </article> 
+
+          <article className="card non-featured">
+            <div className="card-img">
+              <img src={Placeholder2} alt="placeholder" />
+            </div>
+            <div className="card-body">
+              <h4>Solo Project Title</h4>
+              <p><a href="/">Github Repo</a> | <a href="/">Live URL</a></p>
+              <p>html / css / etc</p>
+              <p><span className="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, provident? Velit reprehenderit natus accusantium delectus sed libero soluta voluptate nam architecto, assumenda eveniet harum consequuntur totam unde enim odio deleniti.</span></p>
+            </div>
+          </article> 
         </div>
-      </article>
+      </section>
     );
   }
   
