fa342528b247fb066d33c0da66b0fbe8e1a37383
escowin <edwin@escowinart.com>
2022-05-17 12:38:53 -0500

    header, nav, about | tenative mediaquery setup

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,9 +5,9 @@ function Navigation() {
     <nav>
       <ul className='flex'>
         <li><a href="#about">About</a></li>
-        <li><a href="#solo-work">Solo Work</a></li>
-        <li><a href="#collaborations">Collaborations</a></li>
-        <li><a href="https://github.com/escowin" target="_blank" rel="noreferrer noopener">Github Repo</a></li>
+        <li><a href="#solo-work">Solo<span className="mobile-hide"> Work</span></a></li>
+        <li><a href="#collaborations">Collab<span className="mobile-hide">oration</span>s</a></li>
+        <li><a href="https://github.com/escowin" target="_blank" rel="noreferrer noopener">Github<span className="mobile-hide"> Repo</span></a></li>
         <li><a href="#resume">Resumė</a></li>
       </ul>
     </nav>
