9dd144d165ab896dc36bdd378183fdb9d022b3a2
escowin <edwin@escowinart.com>
2023-07-25 23:21:38 -0500

    eliminated repeating hardcoded pattern in JSX through mapping

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,44 +1,43 @@
 import "./index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faInbox } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import portfolioData from "../../assets/data";
 
 function Contact() {
-  // contacts array is composed of socials & email based on the shared '.com' string value
   let contacts = [];
-  portfolioData.info.links.forEach(link => {
-    if (link.string.includes(".com")) {
-      console.log(link)
-      contacts.push(link)
-    }
-  })
-  console.log(contacts)
+  let modifiedContacts = [];
+  // `icons` array matches index order of corresponding `contacts` array
+  const icons = [faInbox, faGithub, faLinkedin];
 
+  // contacts array is now composed of socials & email based on the shared '.com' string value
+  portfolioData.info.links.forEach((link) => {
+    if (link.string.includes(".com")) {
+      contacts.push(link);
+    }
+  });
+
+  // `modifiedContacts` is composed of objects that retain properties of `contacts` objects, but with a modified `icon` property using the icon variable.
+  for (let i = 0; i < contacts.length; i++) {
+    const iconIndex = i;
+    const updatedContact = { ...contacts[i], icon: icons[iconIndex] };
+    modifiedContacts.push(updatedContact);
+  }
+  console.log(modifiedContacts);
+
+  // mapping the array's objects reduces the need to write repeating code
   return (
     <ul id="contact-links">
-      {/* <li
-        className="icon"
-        onClick={() => window.open(contact.github, "_blank")}
-      >
-        <FontAwesomeIcon icon={faGithub} />
-        <span> github</span>
-      </li>
-      <li
-        className="icon"
-        onClick={() => window.open(contact.linkedin, "_blank")}
-      >
-        <FontAwesomeIcon icon={faLinkedin} />
-        <span> linkedin</span>
-
-      </li>
-      <li
-        className="icon"
-        onClick={() => window.open(`mailto:${contact.email}`)}
-      >
-        <FontAwesomeIcon icon={faEnvelope} />
-        <span> email</span>
-      </li> */}
+      {modifiedContacts.map((contact, i) => (
+        <li
+          key={i}
+          className="icon"
+          onClick={() => window.open(contact.string, "_blank")}
+        >
+          <FontAwesomeIcon icon={contact.icon} />
+          <span> {contact.string}</span>
+        </li>
+      ))}
     </ul>
   );
 }
