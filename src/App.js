266057416a6d84cc9e51a1518d87f91d3a914690
escowin <edwin@escowinart.com>
2023-08-21 15:52:27 -0500

    changed portfolio highlights; rewrote About bio

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
           case "Resume":
             return <Resume />;
           default:
-            return <Portfolio selectedPortfolio={"Frontend"} />;
+            return <About />;
         }
       })()}
       </main>
